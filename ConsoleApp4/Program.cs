3971f9812872b3fbb0facc285097a0249aeb316a
name125asd <name125asd@gmail.com>
2023-10-23 14:37:16 +0700

    first commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int age = 25;
+       
+
+            //string name = "john Smith";
+            //bool isStudent = true;
+            //char grade = 'A';
+            //decimal salary = (decimal)55000.75;
+
+            //Console.WriteLine("{0} {1} {2} {3}", age,Height,name,isStudent );
+
+            //Console.WriteLine("you grade {0}", grade);
+
+            //Console.WriteLine("you monny {0}", salary);
+
+            //if (isStudent)
+            //{
+            //    Console.WriteLine("STUDENT");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NO STUDENT");
+
+
+            //}
+            Console.WriteLine("please enter your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("please enter your age : ");
+            string age = Console.ReadLine();
+
+
+            Console.WriteLine("hello  {0} you age is  {1} year", name, age);
+
+
+
+
+
+
+
+            Console.ReadLine(); // รอผู้ใช้กด Enter ก่อนปิดจอ
+
+
+        }
+
+        static void test()
+        {
+
+
+
+        }
+    }
+}
